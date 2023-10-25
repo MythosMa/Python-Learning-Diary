3def534823aa562241413e8476c5ac358d7d18b0
MYTHOSMA-PC\马原 <mythosmayuan@gmail.com>
2023-10-25 17:59:07 +0800

    update

---
+++ 2023年10月23日/www/config_default.py
@@ -0,0 +1,13 @@
+configs = {
+    'debug': True,
+    'db': {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'root',
+        'password': 'Mayuan112233',
+        'db': 'python_test'
+    },
+    'session': {
+        'secret': 'PythosTest'
+    }
+}
